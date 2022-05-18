d8458454ec11baed8027ac2c6e4aa63c7e02a502
Степан Рохин <step77795@outlook.com>
2022-05-18 16:55:48 +0300

    Составлен шаблон программы

--- Task_apiIO/Program.cs
+++ Task_apiIO/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿/*  Алгоритм 
+ * Составить скелет программы
+ * 
+ */
+using System.Net;
+
+namespace Task_apiIO
+{
+
+    class Programm
+    {
+        static void Main(string [] Args)
+        {
+
+        }
+    }
+
+}
