cc15afc81cb397e47969e35cb60bd5862b1deabe
Степан Рохин <step77795@outlook.com>
2022-05-18 20:38:24 +0300

    Добавил объекты начал их прописывать Добавил реализацию подготовки api ссылки(в зависисмоти от страны). Стал готовить блок кода для вывода

--- Task_apiIO/Program.cs
+++ Task_apiIO/Program.cs
@@ -1,18 +1,58 @@
-﻿/*  Алгоритм 
- * Составить скелет программы
- * 
- */
-using System.Net;
+﻿using System.Net;
 
 namespace Task_apiIO
 {
-
-    class Programm
+    public class World_list_of_uni
     {
-        static void Main(string [] Args)
-        {
+        public string link;
 
+        public void GetList()
+        {
+            
         }
     }
 
+    public class Countries: World_list_of_uni
+    {
+        public string Country_name;
+
+        private void Create_link()
+        {
+            link = link + Country_name;
+        }
+
+
+    }  
+
+
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            World_list_of_uni WList_1 = new World_list_of_uni();
+            WList_1.link = "http://universities.hipolabs.com/search?country=";
+            Countries USA = new Countries();
+            USA.Country_name = "United+States";
+
+            Console.Write("Please enter country name: USA");
+            string s = Console.ReadLine();
+
+            switch (s)
+            {
+                case "usa": 
+                    USA.GetList();
+                    Console.WriteLine();
+                    break;
+                case "": 
+                    Console.WriteLine();
+                    break;
+                case "1":
+                    Console.WriteLine();
+                    break;
+                    default: 
+                    Console.WriteLine();
+                    break;
+            }
+        }
+    }
 }
