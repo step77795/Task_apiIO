68e90e55b16766b9fceb0e45e61ca6bd99a0fe34
Степан Рохин <step77795@outlook.com>
2022-05-18 21:20:45 +0300

    Создал  реализацию API запрос и ответ

--- Task_apiIO/Program.cs
+++ Task_apiIO/Program.cs
@@ -5,11 +5,7 @@ namespace Task_apiIO
     public class World_list_of_uni
     {
         public string link;
-
-        public void GetList()
-        {
-            
-        }
+        
     }
 
     public class Countries: World_list_of_uni
@@ -20,7 +16,21 @@ namespace Task_apiIO
         {
             link = link + Country_name;
         }
+        public string Response { get; set; }
 
+        HttpWebRequest request;
+        public void GetList()
+        {
+            Create_link();
+            request = (HttpWebRequest)WebRequest.Create(link);
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                var stream = response.GetResponseStream();
+                if (stream != null) Response = new StreamReader(stream).ReadToEnd();
+            }
+            catch (Exception) { }
+        }
 
     }  
 
@@ -31,9 +41,11 @@ namespace Task_apiIO
         {
             World_list_of_uni WList_1 = new World_list_of_uni();
             WList_1.link = "http://universities.hipolabs.com/search?country=";
-            Countries USA = new Countries();
-            USA.Country_name = "United+States";
 
+            Countries USA = new Countries();
+
+            USA.Country_name = "United+States";
+            
             Console.Write("Please enter country name: USA");
             string s = Console.ReadLine();
 
