ca6ec0f2832a6e5a4396fe324cb791542932a20c
Степан Рохин <step77795@outlook.com>
2022-05-21 18:26:01 +0300

    Fix № 7,8,11

--- Task_apiIO/Program.cs
+++ Task_apiIO/Program.cs
@@ -3,27 +3,26 @@ using System.Net;
 
 
 namespace Task_apiIO
-{   //Родительский класс(ограничевается раздачей общей кастомной ссылки)
+{
+
+    //Родительский класс(ограничевается раздачей общей кастомной ссылки)
     public class World_list_of_uni
     {
         public string link= "http://universities.hipolabs.com/search?country=";
     }
     //Наследник
     public class Countries: World_list_of_uni
-    {   //Задаёт необходимое название страны для запроса
-        public string Country_name;
-        //Создаёт итоговую ссылку для запроса
-        private void Create_link()
-        {
-            link = link + Country_name;
-        }
+    {
         //Автоматическое свойство. Служит "хабом" для результата выполнения парсинга.
-        protected string Response { get; set; }
+        public string Response { get; set; }
+        //Задаёт необходимое название страны для запроса
+        public string country_name;
+        
         //Метод создания запоса и получения ответа по нему
-        protected void GetJson()
-        {   
-            Create_link();
-            //fix12-> HttpWebRequest request;
+        private void GetJson()
+        {
+            //Создаёт итоговую ссылку для запроса
+            link = link + country_name;
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(link);
             //Попытка вытянуть данные по запросу
             try
@@ -34,13 +33,11 @@ namespace Task_apiIO
             }
             catch (Exception) { Console.WriteLine("Something went wrong( API response = null"); }
         }
-        //Автоматическое свойство. Служит "хабом" для списка университетов по странам.
-        public string short_list { get; set; }
+
         //Метод обеспечивает выборку данных из ответа по запросу(метод GetJson)
-        public void short_list_of_universities()
+        public void Short_list_of_universities()
         {
             GetJson();
-
             /*Json файл приходит в формате 
             * Json
             *      0
@@ -50,13 +47,13 @@ namespace Task_apiIO
             *          ...
             *     ...
             */
-            //Обработка json
+            //Обработка json и вывод name
             var jarray = JArray.Parse(Response);
             for (int i=0; i< jarray.Count; i++)
             {
-                var jarray_i = JObject.Parse(Convert.ToString(jarray[i]));
-                var name = jarray_i["name"];
-                Console.WriteLine(name);
+                    var jarray_i = JObject.Parse(Convert.ToString(jarray[i]));
+                    var name = jarray_i["name"];
+                    Console.WriteLine(name);
             }
         }
     }  
@@ -65,21 +62,19 @@ namespace Task_apiIO
     {
         private static void Main(string[] args)
         {
-            Countries USA = new Countries { Country_name = "United+States"};
-            Countries TR = new Countries { Country_name = "Turkey" };
+            Countries USA = new Countries { country_name = "United+States"};
+            Countries TR = new Countries { country_name = "Turkey" };
 
             Console.Write("Please enter country name( USA or Turkey )  : ");
-            string s = (Console.ReadLine()).ToLower();
+            string str = (Console.ReadLine()).ToLower();
 
-            switch (s)
+            switch (str)
             {
                 case "usa": 
-                    USA.short_list_of_universities();
-                    Console.WriteLine(USA.short_list);
+                    USA.Short_list_of_universities();
                     break;
                 case "turkey":
-                    TR.short_list_of_universities();
-                    Console.WriteLine(TR.short_list);
+                    TR.Short_list_of_universities();
                     break;
                     default: 
                     Console.WriteLine("Unfortunately, we have not found such a country! Try again)");
